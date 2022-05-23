340c7266823ab0e69e33f6566380bd795a56dcb8
GitGab19 <gabriele.vernetti@protonmail.com>
2022-05-23 17:23:40 +0200

    zcash upload script and fixes

--- zcash/zcash_addresses.py
+++ zcash/zcash_addresses.py
@@ -2,15 +2,13 @@ import subprocess
 from sys import stdout
 import json
 
-for x in range(0,1675747):
-    with open('./dash_counter.txt', 'w') as fp:
+for x in range(0,1677816):
+    with open('./zcash_counter.txt', 'w') as fp:
         fp.write(str(x)+'\n')
-    percentage = (x/(1675747))*100
+    percentage = (x/(1677816))*100
     print("block number "+str(x)+" --> "+str(percentage)+"%")
-    with open('./dash_addresses.txt', 'a') as f:
-        block_hash = subprocess.run(["../dash/dashcore-0.17.0/bin/dash-cli", "getblockhash", str(x)], capture_output=True)
-        block_hash_string = block_hash.stdout.decode('utf-8')
-        block = subprocess.run(["../dash/dashcore-0.17.0/bin/dash-cli", "getblock", str(block_hash_string), "2"], capture_output=True)
+    with open('./zcash_addresses.txt', 'a') as f:
+        block = subprocess.run(["../zcash/src/zcash-cli", "getblock", str(x), "2"], capture_output=True)
         block_string = block.stdout.decode('utf-8')
         block_dict = json.loads(block_string)
         txs = block_dict['tx']
@@ -19,7 +17,7 @@ for x in range(0,1675747):
             for v_out in v_outs:
                 if v_out['scriptPubKey']:
                     scriptPubKey = v_out['scriptPubKey']
-                    if (scriptPubKey['type'] != 'nulldata' and scriptPubKey['type'] != 'nonstandard') :
+                    if scriptPubKey['type'] != 'nulldata':
                         addresses = v_out['scriptPubKey']['addresses']
                         for address in addresses:
                             f.write(str(address)+'\n')
