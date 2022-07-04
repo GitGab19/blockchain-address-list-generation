3dacab0d90211e9e4d3b28f630e783d8d1c3bf2c
GitGab19 <gabriele.vernetti@protonmail.com>
2022-07-04 17:10:38 +0200

    updates

--- bitcoin/UncBitcoinGen.py
+++ bitcoin/UncBitcoinGen.py
@@ -1,46 +1,50 @@
+# This script computes the Bitcoin uncompressed addresses  for our starting set and checks if they ever appeared on
+# the blockchain. The whole list of existing addresses must be loaded in advance on a SQL database
+
+import AddressGeneration
 import datetime
 import bitcoin
 import mysql.connector
-import sys
-sys.path.append('../')
-import AddressGeneration
 
+bitcoin.SelectParams('mainnet')
 
 t0 = datetime.datetime.now()
 
+# Luck.txt is the file where the private keys corresponding to existing addresses get saved
 with open("Luck.txt", "a") as f:
     print(t0, file=f)
 
+# Link with the database
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     password="",
-    database="bcash"
+    database="bitcoin" # database name
 )
 
 # Parameters
-
 u = 0
 env = 0
 
 # Name of the file with the 144M keys of our subsets
-file_name = 'secp256k1_keys.txt'
+file_name = 'secp256k1Keys.txt'
 
 fh = open(file_name, 'rt')
 line0 = fh.readline()
-line = fh.readline()
 
+line = fh.readline()
 
 while line:
     chunks = line.split('\t')
-    public_key = AddressGeneration.CompressedPublicKeyComputation(chunks[2], chunks[3])
+    public_key = AddressGeneration.UncompressedPublicKeyComputation(chunks[2], chunks[3])
     public_key = public_key.split('\n')
     private_key = chunks[1]
 
-    pub = AddressGeneration.BCashAddressComputation(public_key[0])
+    pub = AddressGeneration.BitcoinClassicAddressComputation(public_key[0])
 
     mycursor = mydb.cursor()
-    sql = "SELECT * FROM address_bch WHERE address='" + pub + "';"
+    # search in the database: SELECT * FROM tableName WHERE attributeName =
+    sql = "SELECT * FROM addressbtc WHERE address='" + pub + "';"
 
     mycursor.execute(sql)
     myresult = mycursor.fetchone()
@@ -52,6 +56,7 @@ while line:
             print(Message, file=f)
     u = u + 1
     line = fh.readline()
+
 fh.close()
 print(u)
 with open("Luck.txt", "a") as f:
