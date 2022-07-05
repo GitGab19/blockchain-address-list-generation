5ee86edbc8ca9bd4203dd0f6845831ebc2e2daae
Gangi94 <76867383+Gangi94@users.noreply.github.com>
2022-07-05 09:38:41 +0200

    Update SegwitLiteGen.py

--- litecoin/SegwitLiteGen.py
+++ litecoin/SegwitLiteGen.py
@@ -1,10 +1,9 @@
-import datetime
-import bitcoin
+# This script computes the Bitcoin segwit addresses for our starting set and checks if they ever appeared on
+# the blockchain. The whole list of existing addresses must be loaded in advance on a SQL databaseimport datetime
+
 import mysql.connector
 import sys
 sys.path.append('../')
-sys.path.append('/Users/antonio/AppData/Local/Programs/Python/Python39/Lib/site-packages')
-# sys.path.append('/Users/antonio/AppData/Local/Programs/Python/Python39/Lib/site-packages')
 import AddressGeneration
 
 
